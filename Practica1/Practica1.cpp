8c0e9e075b6557728fc6ffee1d2d65fc3a40991b
Jaime Páramo <jaime.paramo@outlook.com>
2024-10-25 20:51:24 +0200

    Practice 1 Finished

--- Practica1/Practica1.cpp
+++ Practica1/Practica1.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 #define MASK_BULLETS 0x00FF0000
 #define MASK4 0xF
@@ -15,20 +14,21 @@ unsigned int GetBulletNumber(unsigned int _uValue) {
 	return uBulletNumber;
 }
 
-void AddBullets(unsigned int _uValue, unsigned int _uBullets) {
+void AddBullets(unsigned int &_uValue, unsigned int _uBullets) {
 	unsigned int uBulletNumber = GetBulletNumber(_uValue);
 
 	uBulletNumber += _uBullets;
+	if (uBulletNumber > UCHAR_MAX) _uBullets = UCHAR_MAX;
 	
+	_uValue &= ~MASK_BULLETS;
 
+	uBulletNumber = (uBulletNumber << 16);
 }
 
 bool AreInfiniteBulletsActive(unsigned int _uValue) {
 	bool bIsActivated = false;
 
 	bIsActivated = _uValue & MASK_INFINITEBULLETS;
-	
-	bIsActivated = bIsActivated >> 1;
 	
 	return bIsActivated;
 }
@@ -40,17 +40,17 @@ void ActivateInfiniteBullets(unsigned int &_uValue) {
 int main() {
 	unsigned int uMyInfo = 0x890D700C;
 	
-	std::cout << "You have " << GetBulletNumber(uMyInfo) << " bullets.\n";
+	printf("You have %u bullets.\n", GetBulletNumber(uMyInfo));
 	
 	AddBullets(uMyInfo, 20);
-	std::cout << "You have " << GetBulletNumber(uMyInfo) << " bullets.\n";
+	printf("You have %u bullets.\n", GetBulletNumber(uMyInfo));
 
-	std::string sInfiniteBullets = AreInfiniteBulletsActive(uMyInfo) ? "Infinite bullets are active." : "Infinite bullets are not active.";
-	std::cout << sInfiniteBullets << '\n';
+	const char* sInfiniteBullets = AreInfiniteBulletsActive(uMyInfo) ? "Infinite bullets are active." : "Infinite bullets are not active.";
+	printf("%s\n", sInfiniteBullets);
 
 	ActivateInfiniteBullets(uMyInfo);
 	sInfiniteBullets = AreInfiniteBulletsActive(uMyInfo) ? "Infinite bullets are active." : "Infinite bullets are not active.";
-	std::cout << sInfiniteBullets << '\n';
+	printf("%s\n", sInfiniteBullets);
 
-	return 0;
+	return 0; 
 }
