b82a00192d7569b4fe1d7012b4fa4518fbb289a3
Jaime Páramo <jaime.paramo@outlook.com>
2024-10-29 21:36:57 +0100

    Test Updated

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,17 +1,11 @@
 #include <iostream>
 
-struct SBool {
-	bool bBool1;
-	bool bBool2;
-	bool bBool3;
-	bool bBool4;
-};
-
-struct SInt {
-	int iNumber;
-};
-
 int main() {
-	printf("%u\n", static_cast<unsigned int>(sizeof(SBool)));
-	printf("%u\n", static_cast<unsigned int>(sizeof(SInt)));
+	unsigned char half_limit = 150;
+
+	for (unsigned char i = 0; i < 2 * half_limit; i++) {
+
+	}
+
+	return 0;
 }
