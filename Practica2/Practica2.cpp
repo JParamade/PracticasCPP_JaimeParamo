47ce43ef8a5bb7308de914e88c78ff1a2a175950
JParamade <114541314+JParamade@users.noreply.github.com>
2024-11-30 17:54:03 +0100

    Updated Practice 2

--- Practica2/Practica2.cpp
+++ Practica2/Practica2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 void IntegerBytes(unsigned int _uNumber) {
 	unsigned char* pByte = reinterpret_cast<unsigned char*>(&_uNumber);
@@ -12,8 +11,8 @@ void GetBiggestInteger(int _iTable[], const unsigned int _uTableSize) {
 
 	int iBiggestInteger = INT_MIN;
 
-	for (unsigned int uIndex = 0u; uIndex < _uTableSize; uIndex++) 
-		if (iBiggestInteger < *(pTable + uIndex)) 
+	for (unsigned int uIndex = 0u; uIndex < _uTableSize; uIndex++)
+		if (iBiggestInteger < *(pTable + uIndex))
 			iBiggestInteger = *(pTable + uIndex);
 
 	printf("The biggest integer is %d.\n", iBiggestInteger);
@@ -23,31 +22,40 @@ void GetBiggestByte(int _iTable[], const unsigned int _uTableSize) {
 	unsigned char* pTable = reinterpret_cast<unsigned char*>(_iTable);
 
 	unsigned char iBiggestByte = 0;
-	
+
 	for (unsigned int uIndex = 0u; uIndex < _uTableSize; uIndex++)
 		if (iBiggestByte < *(pTable + uIndex))
 			iBiggestByte = *(pTable + uIndex);
-		
+
 	printf("The biggest byte is %02hhx.\n", iBiggestByte);
 }
 
-const char* InvertString(const char* _sString) {
+void InvertString(char* _sString) {
+	char cAux;
+	char* pStringBegin = _sString;
+	char* pStringEnd = pStringBegin + strlen(_sString) - 1;
 
+	while (pStringBegin < pStringEnd) {
+		cAux = *pStringBegin;
+		*pStringBegin = *pStringEnd;
+		*pStringEnd = cAux;
 
-	return "";
+		pStringBegin++;
+		pStringEnd--;
+	}
 }
 
 int main() {
 	unsigned int uNumber = 7;
 	IntegerBytes(uNumber);
 
-	int iTable[] = {1, 3, 2, 5, 3, static_cast<int>(0xFFFFFFFF), 2};
+	int iTable[] = { 1, 3, 2, 5, 3, static_cast<int>(0xFFFFFFFF), 2 };
 	const unsigned int iTableSize = sizeof(iTable) / sizeof(int);
 	GetBiggestInteger(iTable, iTableSize);
 	GetBiggestByte(iTable, sizeof(iTable));
-	
-	const char* sInvertedString = InvertString("ads");
-	
+
+	char sInvertedString[] = "This string is getting inverted.";
+	InvertString(sInvertedString);
 	printf("%s\n", sInvertedString);
 
 	return 0;
