717ceb461eb885fd6cdb2c645cd58e8eb7493473
Jaime Páramo <jaime.paramo@outlook.com>
2024-12-03 21:46:07 +0100

    Updated Practice 5

--- Practica5/Practica5.cpp
+++ Practica5/Practica5.cpp
@@ -2,21 +2,23 @@
 #include "FileUtils.h"
 
 int main() {
+	// Read File
 	void* pFile = OpenFile("Test.txt", "r");
 
 	const unsigned int uBufferSize = 128;
 	char sReadBuffer[uBufferSize];
 
-	unsigned int uCharsRead = ReadFile(sReadBuffer, uBufferSize, pFile);
+	unsigned int uCharsRead = static_cast<unsigned int>(ReadFile(sReadBuffer, uBufferSize, pFile));
 	printf("%u characters have been read.\n", uCharsRead);
 
 	printf("%s\n", CloseFile(pFile) ? "File closed correctly." : "File failed to close.");
 
-	pFile = OpenFile("Test.txt", "w");
+	// Write File
+	pFile = OpenFile("Test.txt", "a");
 
 	char sWriteBuffer[] = "Hola";
 
-	unsigned int uCharsWritten = WriteFile(sWriteBuffer, sizeof(sWriteBuffer), pFile);
+	unsigned int uCharsWritten = static_cast<unsigned int>(WriteFile(sWriteBuffer, sizeof(sWriteBuffer), pFile));
 	printf("%u characters have been written.\n", uCharsWritten);
 
 	printf("%s\n", CloseFile(pFile) ? "File closed correctly." : "File failed to close.");
