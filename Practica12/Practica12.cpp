2012acae54daec73a150cc3958a68d176ced1e1d
JParamade <114541314+JParamade@users.noreply.github.com>
2025-01-05 04:46:56 +0100

    Updated Practice 12

--- Practica12/Practica12.cpp
+++ Practica12/Practica12.cpp
@@ -2,9 +2,20 @@
    WORK IN PROGRESS
 ********************/
 
+/********************
+   12. (OPCIONAL) Operadores:
+		a. Dada la clase “CString” declarada en el fichero “String.h” implementar en un
+		fichero .cpp toda la funcionalidad de dicha clase.
+		b. Mejora: Implementar funcionalidad para que se puedan realizar operaciones
+		de tipo:
+			i. CString str(“hola ”);
+			ii. CString str2 = “Hola“ + str + “caracola”
+********************/
+
 #include <iostream>
-using namespace std;
 
 int main() {
+
+
 	return 0;
 }
--- Practica12/String.cpp
+++ Practica12/String.cpp
@@ -1,2 +1,18 @@
 #include "String.h"
 
+CString::CString(const char* str = "") {
+	unsigned int uStringSize = strlen(str ? str : "");
+	m_p = new char(uStringSize);
+	strcpy_s(static_cast<char*>(m_p), uStringSize, str ? str : "");
+}
+
+CString::CString(const CString& str) {
+	const char* sSource = static_cast<const char*>(str.m_p);
+	unsigned int uStringSize = strlen(sSource);
+	m_p = new char(uStringSize);
+	strcpy_s(static_cast<char*>(m_p), uStringSize, sSource);
+}
+
+CString::~CString() {
+	delete[] static_cast<char*>(m_p);
+}
